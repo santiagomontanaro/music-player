a7c800dc35ebfe0e55190e62ad41c2758d839ed9
Santiago Montanaro <70220938+santiagomontanaro@users.noreply.github.com>
2021-05-18 21:59:25 -0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,140 @@
+let allMusic = [
+    {
+        name: "An emotional sad song",
+        artist: "Nickel's",
+        img: "an-emotional-sad-song",
+        src: "an-emotional-sad-song"
+    },
+    {
+        name: "Lovely (slowed down)",
+        artist: "Billie Eilish, Khalid",
+        img: "billie-eilish-khalid-lovely",
+        src: "billie-eilish-khalid-lovely"
+    },
+    {
+        name: "Violent (Just A Gent Remix)",
+        artist: "carolesdaughter",
+        img: "carolesdaughter-violent-remix",
+        src: "carolesdaughter-violent-remix"
+    },
+    {
+        name: "AMNESIA (feat. Boy In Space)",
+        artist: "DREAMDNVR",
+        img: "dreamdnvr-amnesia",
+        src: "dreamdnvr-amnesia"
+    },
+    {
+        name: "Drugs",
+        artist: "Falling In Reverse",
+        img: "falling-in-reverse-drugs",
+        src: "falling-in-reverse-drugs"
+    },
+    {
+        name: "All Outta Oil (with DEPTH STRIDA)",
+        artist: "Istasha",
+        img: "istasha-all-outta-oil",
+        src: "istasha-all-outta-oil"
+    },
+    {
+        name: "In A Sea Of Razors Ft.Na$tii ",
+        artist: "Istasha",
+        img: "istasha-in-a-sea-of-razors",
+        src: "istasha-in-a-sea-of-razors"
+    },
+    {
+        name: "Neon Junction/Germ Cell Evacuation ",
+        artist: "Istasha",
+        img: "istasha-neon-junction",
+        src: "istasha-neon-junction"
+    },
+    {
+        name: "RLD ",
+        artist: "Istasha",
+        img: "istasha-rld",
+        src: "istasha-rld"
+    },
+    {
+        name: "Snort Cocaine For Jesus ",
+        artist: "Istasha",
+        img: "istasha-snort-cocaine",
+        src: "istasha-snort-cocaine"
+    },
+    {
+        name: "White Noise (w. DEPTH STRIDA)",
+        artist: "Istasha",
+        img: "istasha-white-noise",
+        src: "istasha-white-noise"
+    },
+    {
+        name: "CANVAS これを翻訳しました",
+        artist: "Istasha",
+        img: "istasha-canvas",
+        src: "istasha-canvas"
+    },
+    {
+        name: "Lost Days ",
+        artist: "Istasha",
+        img: "istasha-lost-days",
+        src: "istasha-lost-days"
+    },
+    {
+        name: "Spinal Tap",
+        artist: "Istasha",
+        img: "istasha-spinal-tap",
+        src: "istasha-spinal-tap"
+    },
+    {
+        name: "THE TRAIL RUNS COLD (with MUGXTSU)",
+        artist: "Istasha",
+        img: "istasha-the-trail-runs-cold",
+        src: "istasha-the-trail-runs-cold"
+    },
+    {
+        name: "Robbery",
+        artist: "Juice WRLD",
+        img: "juice-wrld-robbery",
+        src: "juice-wrld-robbery"
+    },
+    {
+        name: "Paparazzi",
+        artist: "Kim Dracula",
+        img: "kim-dracula-paparazzi",
+        src: "kim-dracula-paparazzi"
+    },
+    {
+        name: "Astronaut In The Ocean",
+        artist: "Masked Wolf",
+        img: "masked-wolf-astronaut-in-the-ocean",
+        src: "masked-wolf-astronaut-in-the-ocean"
+    },
+    {
+        name: "Eu Achei (Juelz Remix)",
+        artist: "MC Paulin da Capital",
+        img: "mc-paulin-da-capital-eu-achei",
+        src: "mc-paulin-da-capital-eu-achei"
+    },
+    {
+        name: "End of the Road (Stereotype Remix)",
+        artist: "Noga Erez",
+        img: "noga-erez-end-of-the-road",
+        src: "noga-erez-end-of-the-road"
+    },
+    {
+        name: "Just Do It [Ultimate Remix]",
+        artist: "Shia LaBeouf",
+        img: "shia-lebouf-just-do-it",
+        src: "shia-lebouf-just-do-it"
+    },
+    {
+        name: "Sweater Weather",
+        artist: "The Neighbourhood",
+        img: "the-neightbourhood-sweater-weather",
+        src: "the-neightbourhood-sweater-weather"
+    },
+    {
+        name: "The Business (T-Mass Remix)",
+        artist: "Tiësto",
+        img: "tiesto-the-business",
+        src: "tiesto-the-business"
+    }
+]
